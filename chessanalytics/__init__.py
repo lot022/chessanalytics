8f0ad09a33e6ef5aa9106f5588b384565a65b0e8
lot022 <uhppow@proton.me>
2024-07-17 20:15:09 +0200

    Update __init__.py

--- chessanalytics/__init__.py
+++ chessanalytics/__init__.py
@@ -1,2 +1,11 @@
 from .chessanalytics import CA
 from .chessanalytics_streamlit import CAST
+from .chessanalytics_api import API
+from .chessanalytics_tools import Tools
+
+__all__ = [
+    "CA",
+    "CAST",
+    "API",
+    "TOOLS",
+]
